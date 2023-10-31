47a9d68e3c8c455eaa5c4950593ea8f2b26b7bc1
zheyang0825 <108724832+zheyang0825@users.noreply.github.com>
2023-10-30 21:12:48 -0500

    Add capsule manager apis (#8)
    * Create cla.yml
    
    * Create trigger-circleci.yml
    
    * repo-sync-2023-10-30T16:05:08+0800
    
    * rm files
    
    * fix comment

--- python/setup.py
+++ python/setup.py
@@ -1,23 +1,25 @@
 import setuptools
 import os
 
+
 def read(fname):
     with open(os.path.join(os.path.dirname(__file__), fname)) as f:
         return f.read()
 
+
 setuptools.setup(
-    name = "sdc-apis",
-    version = "0.1.0b0",
-    author = "secretflow",
-    author_email = "secretflow-contact@service.alipay.com",
-    description = "SecretFlow Data Capsule apis proto generated python",
-    long_description_content_type = 'text/markdown',
-    long_description = 'SecretFlow Data Capsule apis proto generated python',
-    license = "Apache 2.0",
-    url = "https://github.com/secretflow/secure-data-capsule-apis",
-    packages = setuptools.find_packages(),
-    install_requires = read('requirements.txt'),
-    classifiers = [
+    name="sdc-apis",
+    version="0.2.0.dev20230930",
+    author="secretflow",
+    author_email="secretflow-contact@service.alipay.com",
+    description="SecretFlow Data Capsule apis proto generated python",
+    long_description_content_type="text/markdown",
+    long_description="SecretFlow Data Capsule apis proto generated python",
+    license="Apache 2.0",
+    url="https://github.com/secretflow/secure-data-capsule-apis",
+    packages=setuptools.find_packages(),
+    install_requires=read("requirements.txt"),
+    classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
