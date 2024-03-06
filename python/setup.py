71c52f21790636733400b9e478e88db69eabb294
zheyang0825 <yz262713@antgroup.com>
2024-03-06 10:48:27 +0800

    feat: add cargo support

--- python/setup.py
+++ python/setup.py
@@ -9,7 +9,7 @@ def read(fname):
 
 setuptools.setup(
     name="sdc-apis",
-    version="0.2.0.dev20230930",
+    version="0.2.1.dev20240305",
     author="secretflow",
     author_email="secretflow-contact@service.alipay.com",
     description="SecretFlow Data Capsule apis proto generated python",
@@ -24,5 +24,8 @@ setuptools.setup(
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ],
+    options={
+        "bdist_wheel": {"plat_name": "manylinux2014_x86_64"},
+    },
     include_package_data=True,
 )
