cea800bff6f02d7471de8748c2e8e9ce6aa18e56
anakinxc <103552181+anakinxc@users.noreply.github.com>
2024-03-20 15:13:42 +0800

    Cherry-pick [Infra] Upgrade Protobuf (#13) (#14)
    [Infra] Upgrade Protobuf (#13)
    
    * Update protobuf
    
    * Add pb version
    
    * Relax pb

--- python/setup.py
+++ python/setup.py
@@ -9,7 +9,7 @@ def read(fname):
 
 setuptools.setup(
     name="sdc-apis",
-    version="0.2.0.dev20230930",
+    version="0.2.0.dev20240320",
     author="secretflow",
     author_email="secretflow-contact@service.alipay.com",
     description="SecretFlow Data Capsule apis proto generated python",
